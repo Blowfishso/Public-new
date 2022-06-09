6fefe9894d97ff68dde10df3a52a02448e94d2ae
Blowfishso <fvi345@gmail.com>
2022-06-09 13:23:57 +0300

    A new file is added

--- Task 12.7.3.py
+++ Task 12.7.3.py
@@ -2,13 +2,13 @@ money = float(input('Введите сумму вклада: '))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
 b1 = per_cent.get('ТКБ')
-Bank1 = round(b1/100*money)
+Bank1 = round(b1 / 100 * money)
 b2 = per_cent.get('СКБ')
-Bank2 = round(b2/100*money)
+Bank2 = round(b2 / 100 * money)
 b3 = per_cent.get('ВТБ')
-Bank3 = round(b3/100*money)
+Bank3 = round(b3 / 100 * money)
 b4 = per_cent.get('СБЕР')
-Bank4 = round(b4/100*money)
+Bank4 = round(b4 / 100 * money)
 
 deposit = [Bank1, Bank2, Bank3, Bank4]
 max_percent = max(deposit)
